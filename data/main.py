505c6eb629fa1656ce73936a62af63bd95618910
Michael Paterson <michaelpaterson194@gmail.com>
2022-11-14 19:15:46 +0000

    _

--- data/main.py
+++ data/main.py
@@ -1,6 +1,18 @@
-import json
+import csv
 
-with open("data\poi.json") as json_file:
-    poi = json.load(json_file)
+with open ("poi.csv", "a") as file:
+    csv_reader = csv.reader(file)
+    headings = next(csv_reader)
 
-print(poi)
+
+class point_of_interest():
+    def __init__(self):
+        self.MAX = 10
+        self.arr = [None for x in range(self.MAX)]
+
+    def get_poi(self, key):
+        h = 0
+        for char in key:
+            h += ord(char)
+        return h % self.MAX
+    
